55c48197c9bdf1d407037cdf11cd454fa92c2acf
kathir2602 <kathir@crayond.co>
2022-02-02 20:31:14 +0530

    init

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    id: 1,
+    title: 'what is your name?',
+    info: 'ray d code',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info: 'delete app',
+  },
+  {
+    id: 3,
+    title: 'What is ID?',
+    info: 'Identificaion',
+  },
+  
+];
+export default questions;
